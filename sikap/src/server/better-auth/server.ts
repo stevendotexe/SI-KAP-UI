9c0d45924d0e1e5c154922c22cb48e14277e0732
Andika E. Kurnia <73420758+DikDns@users.noreply.github.com>
2025-11-29 16:48:23 +0700

    feat: add file storage server action function

--- sikap/src/server/better-auth/server.ts
+++ sikap/src/server/better-auth/server.ts
@@ -2,6 +2,6 @@ import { auth } from ".";
 import { headers } from "next/headers";
 import { cache } from "react";
 
-export async function getSession() {
-  return auth.api.getSession({ headers: await headers() });
-}
+export const getSession = cache(async () =>
+  auth.api.getSession({ headers: await headers() }),
+);
